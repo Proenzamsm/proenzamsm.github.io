290bf36c4ee63ff7f3458e349886ff18aa664c08
Proenzamsm <proezamelhorasaudemental@yahoo.com>
2026-09-15 23:15:53 +0100

    Add files via upload

---
+++ dados/menu.js
@@ -0,0 +1,8 @@
+window.SITE_MENU = [
+  { label: "Início", href: "index.html" },
+  { label: "Sobre o Projeto", href: "projeto.html" },
+  { label: "Objetivos", href: "objetivos.html" },
+  { label: "O que Fazemos", href: "atividades.html" },
+  { label: "Resultados", href: "resultados.html" },
+  { label: "Contactos", href: "contactos.html" }
+];
