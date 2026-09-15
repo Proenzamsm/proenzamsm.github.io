ccc8797b0aa5709c7a0adaf854ddd57e818e232a
Proenzamsm <proezamelhorasaudemental@yahoo.com>
2026-09-15 23:15:11 +0100

    Delete dados directory

--- dados/contactos.js
+++
@@ -1,1 +0,0 @@
-window.PAGE_DATA = { eyebrow:"FALE CONNOSCO", heading:"Contactos", intro:"Estamos disponíveis para partilhar informação sobre o projeto e as suas atividades.", project:"Proenza Melhora a Saúde Mental", code:"2024-2-PT01-KA122-ADU-000290918", location:"Região Autónoma da Madeira · Portugal", email:"proeza@example.com" };
