338d27a1acb5e07148045d2062786ff143a1c022
proezamsm <proezamelhorasaudemental@yahoo.com>
2026-09-16 16:57:36 +0100

    Rename menu item 'O que Fazemos' to 'Atividades'

--- dados/menu.js
+++ dados/menu.js
@@ -2,7 +2,7 @@ window.SITE_MENU = [
   { label: "Início", href: "index.html" },
   { label: "Projeto", href: "projeto.html" },
   { label: "Objetivos", href: "objetivos.html" },
-  { label: "O que Fazemos", href: "atividades.html" },
+  { label: "Atividades", href: "atividades.html" },
   { label: "Resultados", href: "resultados.html" },
    { label: "Mobilidades", href: "mobilidades.html" },
   { label: "Contactos", href: "contactos.html" }
