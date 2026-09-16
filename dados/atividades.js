e436c5ec6e54d5cee31a5a8016faa01867b003b2
Proenzamsm <proezamelhorasaudemental@yahoo.com>
2026-09-16 01:34:29 +0100

    Refactor image entries in atividades.js

--- dados/atividades.js
+++ dados/atividades.js
@@ -2,67 +2,27 @@ window.PAGE_DATA = {
   eyebrow: "GALERIA",
   heading: "Momentos do projeto",
   intro: "Imagens das atividades, formações e momentos de partilha do projeto Erasmus+.",
+
   images: [
-    {
-      src: "css/imagens/logo.png",
-      alt: "Atividade de formação do projeto"
-    },
-    {
-      src: "css/imagens/logo.png",
-      alt: "Momento de aprendizagem e partilha"
-    },
-    {
-      src: "css/imagens/logo.png",
-      alt: "Participantes numa atividade do projeto"
-    },
-    {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-        {
-      src: "css/imagens/logo.png",
-      alt: "Momento de convívio entre participantes"
-    },
-    
-    {
-      src: "css/imagens/galeria-05.jpg",
-      alt: "Atividade relacionada com o bem-estar"
-    },
-    {
-      src: "css/imagens/galeria-06.jpg",
-      alt: "Momento de formação Erasmus+"
-    }
+    { src: "css/imagens/logo.png", alt: "Imagem 01 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 02 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 03 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 04 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 05 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 06 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 07 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 08 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 09 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 10 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 11 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 12 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 13 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 14 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 15 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 16 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 17 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 18 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 19 do projeto" },
+    { src: "css/imagens/logo.png", alt: "Imagem 20 do projeto" }
   ]
 };
