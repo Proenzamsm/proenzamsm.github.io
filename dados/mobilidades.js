5e98a5340f64433c2c6c352220920d81235f6235
Proenzamsm <proezamelhorasaudemental@yahoo.com>
2026-09-16 11:53:36 +0100

    Add mobilities data for Erasmus project
    Added data for three mobilities including titles, locations, descriptions, and images.

--- dados/mobilidades.js
+++ dados/mobilidades.js
@@ -1,1 +1,46 @@
+window.PAGE_DATA = {
+  eyebrow: "MOBILIDADES · ERASMUS+",
+  heading: "As nossas mobilidades",
+  intro: "Conheça as experiências de aprendizagem, formação e partilha realizadas no âmbito do projeto Erasmus+.",
 
+  mobilities: [
+    {
+      title: "Mobilidade 01",
+      location: "Local da mobilidade",
+      description: "Descrição da mobilidade, das atividades realizadas e da experiência dos participantes.",
+      images: [
+        "css/imagens/mobilidades/mobilidade-01-01.jpg",
+        "css/imagens/mobilidades/mobilidade-01-02.jpg",
+        "css/imagens/mobilidades/mobilidade-01-03.jpg",
+        "css/imagens/mobilidades/mobilidade-01-04.jpg",
+        "css/imagens/mobilidades/mobilidade-01-05.jpg"
+      ]
+    },
+
+    {
+      title: "Mobilidade 02",
+      location: "Local da mobilidade",
+      description: "Descrição da mobilidade, das atividades realizadas e da experiência dos participantes.",
+      images: [
+        "css/imagens/mobilidades/mobilidade-02-01.jpg",
+        "css/imagens/mobilidades/mobilidade-02-02.jpg",
+        "css/imagens/mobilidades/mobilidade-02-03.jpg",
+        "css/imagens/mobilidades/mobilidade-02-04.jpg",
+        "css/imagens/mobilidades/mobilidade-02-05.jpg"
+      ]
+    },
+
+    {
+      title: "Mobilidade 03",
+      location: "Local da mobilidade",
+      description: "Descrição da mobilidade, das atividades realizadas e da experiência dos participantes.",
+      images: [
+        "css/imagens/mobilidades/mobilidade-03-01.jpg",
+        "css/imagens/mobilidades/mobilidade-03-02.jpg",
+        "css/imagens/mobilidades/mobilidade-03-03.jpg",
+        "css/imagens/mobilidades/mobilidade-03-04.jpg",
+        "css/imagens/mobilidades/mobilidade-03-05.jpg"
+      ]
+    }
+  ]
+};
